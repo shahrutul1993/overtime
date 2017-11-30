ded0f0357a4f150b831fb9bc7fb5c3d2e3fd6704
Shah RUtul <shahrutul111@gmail.com>
2017-11-30 19:35:56 +0530

    added crud functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	before_action :set_post, only: [:show]
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post, notice: "Your post is successfully created."
+		else
+			render :new
+	end
+
+	def show
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:date, :rationale)
+	end
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+end
