9466c29cb4482ad7f4ab739648388717109a72ac
Shah RUtul <shahrutul111@gmail.com>
2017-12-04 17:09:48 +0530

    imepemented approved post and hiding edit button after approved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,5 @@ module ApplicationHelper
 	def active?(path)
 		"active" if current_page?(path)
 	end
+	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
 	enum status: {submitted: 0, approved: 1, rejected: 2}
 	validates_presence_of :date, :rationale
 	belongs_to :user
+	scope :posts_by, ->(user) {where(user_id: user.id)}
 end
